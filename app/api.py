b5d57da42dfeb17f63438d941cf65754541d2d4b
Johannes Jansson <johannes.e.jansson@gmail.com>
2023-02-06 00:19:27 +0100

    Prevent duplicate rows in weights

--- app/api.py
+++ app/api.py
@@ -92,6 +92,9 @@ class Weight(Base):
                         nullable=False, server_default=text("now()"))
     updated_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text("now()"))
+    __table_args__ = (
+        db.UniqueConstraint('user_id', 'date',),
+    )
 
 
 # Routes
